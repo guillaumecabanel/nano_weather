594e827b7110813e7ee5e7e7081c62593836e452
Guillaume Cabanel <guillaume.cabanel@gmail.com>
2022-03-20 21:43:29 +0100

    add lcd display

--- application.rb
+++ application.rb
@@ -4,6 +4,8 @@ require_relative 'lib/meteo_france'
 require 'sinatra'
 require 'sinatra/json'
 
+RAIN_INTENSITIES = ["  ", "░░", "▒▒", "▓▓", "██"]
+
 get('/') do
   erb :index
 end
@@ -13,4 +15,25 @@ get('/:latitude/:longitude/rain') do
 
   status 422 if response[:error]
   json response
+end
+
+get('/:latitude/:longitude/rain_text') do
+  response = MeteoFrance.new(params[:latitude], params[:longitude]).rain
+
+  cells = response[:forecast].map do |cell|
+    RAIN_INTENSITIES[cell[:rain_intensity].to_i]
+  end
+
+  minutes = response[:forecast].map do |cell|
+    cell[:time].localtime.strftime("%M")
+  end
+
+  status 422 if response[:error]
+
+  <<~TXT
+    ☀#{RAIN_INTENSITIES.join}☂#{' ' * (26 - 17)}#{Time.now.localtime.strftime("%H:%M")}
+    #{'─' * (26)}
+    #{minutes.join(" ")}
+    #{cells.join(" ")}
+  TXT
 end
